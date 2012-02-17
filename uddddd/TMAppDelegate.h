12d8999b5971027857579d78505378c76ce01ec9
mtakagi <mtakagi127@live.jp>
2012-02-17 13:07:18 +0900

    Initial Commit

---
+++ uddddd/TMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TMAppDelegate.h
+//  uddddd
+//
+//  Created by TAKAGI MASAYA on 12/02/17.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ uddddd/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  uddddd
+//
+//  Created by TAKAGI MASAYA on 12/02/17.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TMAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TMAppDelegate class]));
+    }
+}
